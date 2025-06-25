221924d5deac9723ae5c339bff17811c2bf0351a
Brett <brett@webe.au>
2025-06-26 08:14:27 +0930

    feat(og) og

---
+++ src/components/svg/iconOld.svelte
@@ -0,0 +1,16 @@
+<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 256">
+	<circle cx="128" cy="128" r="64" fill="#00a4cc" />
+	<path
+		d="M96 128 L160 128 M144 112 L160 128 L144 144"
+		stroke="white"
+		stroke-width="16"
+		stroke-linecap="round"
+		stroke-linejoin="round"
+	/>
+	<path
+		d="M64 64 L192 64 M64 192 L192 192"
+		stroke="white"
+		stroke-width="16"
+		stroke-linecap="round"
+	/>
+</svg>
